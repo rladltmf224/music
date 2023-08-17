3a186fe65d45b6b1928cb9a064e149991976e7b6
Yiseul Kim <yiseulkim@Yiseului-MacBookPro.local>
2023-08-17 19:14:48 +0900

    feat:음악플레이어 컴포넌트

--- src/components/PlayBlock.js
+++ src/components/PlayBlock.js
@@ -6,6 +6,10 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import Button from "@mui/material/Button";
+import Slider from "@mui/material/Slider";
+import MusicPlayerSlider from "./player/MUI";
+
 import "../App.css";
 
 const Top = styled.div`
@@ -17,27 +21,33 @@ function PlayerBlock(props) {
   const artist = "밤편지";
 
   return (
+    // <div className="PlayerBlock">
+    //   <Top>
+    //     <CircleButton
+    //       float={{ float: "left", "margin-top": "1rem", "margin-left": "1rem" }}
+    //       icon=<IoMdArrowRoundBack />
+    //     />
+    //     <div>
+    //       <CircleButton
+    //         float={{
+    //           float: "right",
+    //           "margin-top": "1rem",
+    //           "margin-right": "1rem",
+    //         }}
+    //         icon=<GiHamburgerMenu />
+    //       />
+    //     </div>
+    //   </Top>
+    //   <CdBlock />
+
+    //   <Button variant="contained">Hello World</Button>
+    //   <Slider max={10} min={0} valueLabelDisplay="auto" />
+    //   <SongInfo title={title} artist={artist} />
+    //   {/* <RangeBar /> */}
+    //   <PlayButton />
+    // </div>
     <div className="PlayerBlock">
-      <Top>
-        <CircleButton
-          float={{ float: "left", "margin-top": "1rem", "margin-left": "1rem" }}
-          icon=<IoMdArrowRoundBack />
-        />
-        <div>
-          <CircleButton
-            float={{
-              float: "right",
-              "margin-top": "1rem",
-              "margin-right": "1rem",
-            }}
-            icon=<GiHamburgerMenu />
-          />
-        </div>
-      </Top>
-      <CdBlock />
-      <SongInfo title={title} artist={artist} />
-      {/* <RangeBar /> */}
-      <PlayButton />
+      <MusicPlayerSlider></MusicPlayerSlider>
     </div>
   );
 }
